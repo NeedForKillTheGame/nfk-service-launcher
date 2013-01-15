679e51eb7d34af651a6630d18873785e5f0e028b
HarpyWar <harpywar@gmail.com>
2013-01-15 22:45:12 +0400

    console parameters

--- Program.cs
+++ Program.cs
@@ -4,141 +4,49 @@ using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading;
 
 namespace nfkdedic
 {
     class Program
     {
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
-        [DllImport("user32.dll")]
-        public static extern int SendMessage(IntPtr hWnd, int msg, int param, System.Text.StringBuilder text);
-        [DllImport("user32.dll")]
-        private static extern int ShowWindow(IntPtr hwnd, int nCmdShow);
-
-        const int SW_HIDE = 0;
-
-        const int WM_GETTEXT = 0x0D;
-        const int WM_GETTEXTLENGTH = 0x0E;
-        const int WM_SETTEXT = 0x000C;
-        const int WM_COMMAND = 0xf5;
-
-        private static Process process;
-
-        private static IntPtr mainHandle;
-        private static IntPtr textHandle;
-        private static IntPtr inputHandle;
-        private static IntPtr sendHandle;
 
 
 
         static void Main(string[] args)
         {
-            Config.ServerExeFile = @"m:\SERVERS\NFK\Server_1-All\Server_1-All\Server.dat";
+            if (args.Length != 1)
+            {
+                Console.WriteLine("usage: nfkdedic.exe [server.dat]");
+            }
+
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("'{0}' doesn't exists!", args[0]);
+                return;
+            }
+
+            Config.ServerExeFile = args[0];
 
 
             // TODO: register console close event
             AppDomain.CurrentDomain.ProcessExit += new EventHandler(CurrentDomain_ProcessExit);
 
-            Start();
 
 
+            Server.Start();
 
             while (true)
             {
-                SendCommand(Console.ReadLine());
+                Server.SendCommand(Console.ReadLine());
             }
         }
 
 
-        // start nfk dedicated in new thread
-        private static void Start()
-        {
-            var t = new Thread(StartNFK);
-            t.Start();
-        }
-
-        [STAThread]
-        private static void StartNFK()
-        {
-            // TODO: run without window on start
-            process = new Process
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = Config.ServerExeFile,
-                    WorkingDirectory = Path.GetDirectoryName(Config.ServerExeFile),
-                    Arguments = "+gowindow +nosound +nfkplanet +game server +exec server +dontsavecfg software",
-                    RedirectStandardOutput = false,
-                    UseShellExecute = false,
-                    WindowStyle = ProcessWindowStyle.Minimized
-                }
-            };
-
-            process.Start();
-
-            // wait a second
-            Thread.Sleep(1000);
-
-
-            // get control handles
-            mainHandle = process.MainWindowHandle;
-
-            ShowWindow(mainHandle, SW_HIDE); // hide main window
-            textHandle = FindWindowEx(mainHandle, new IntPtr(0), "TMemo", null);
-            inputHandle = FindWindowEx(mainHandle, new IntPtr(0), "TEdit", null);
-            sendHandle = FindWindowEx(mainHandle, new IntPtr(0), "TButton", null);
-
-
-            // write console log to file
-            while (!process.HasExited)
-            {
-                Log.Push(GetOutput(textHandle));
-
-                // second interfal to fetch log from the NFK window
-                Thread.Sleep(1000);
-            }
-
-
-            Log.Error("Server crashed! Restarting...");
-            Thread.Sleep(3000);
-            Log.ClearOldText();
-            Start();
-        }
-
-        /// <summary>
-        /// Send command to NFK console
-        /// </summary>
-        /// <param name="text"></param>
-        private static void SendCommand(string text)
-        {
-            // fill input
-            SendMessage(inputHandle, WM_SETTEXT, 0, new StringBuilder(text));
-            // click send
-            SendMessage(sendHandle, WM_COMMAND, 0, null);
-        }
-
-        /// <summary>
-        /// Return output from NFK console
-        /// </summary>
-        /// <param name="hWnd"></param>
-        /// <returns></returns>
-        private static string GetOutput(IntPtr hWnd)
-        {
-            int Len = SendMessage(hWnd, WM_GETTEXTLENGTH, 0, null) + 1;
-
-            var text = new System.Text.StringBuilder(Len);  // or length from call with GETTEXTLENGTH
-            int RetVal = SendMessage(hWnd, WM_GETTEXT, text.Capacity, text);
-
-            return text.ToString();
-        }
+        
 
         static void CurrentDomain_ProcessExit(object sender, EventArgs e)
         {
-            // kill nfk on program exit
-            if (!process.HasExited)
-                process.Kill();
+
 
             Environment.Exit(0);
         }
---
+++ Server.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace nfkdedic
+{
+    class Server
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int msg, int param, System.Text.StringBuilder text);
+        [DllImport("user32.dll")]
+        private static extern int ShowWindow(IntPtr hwnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+
+        const int WM_GETTEXT = 0x0D;
+        const int WM_GETTEXTLENGTH = 0x0E;
+        const int WM_SETTEXT = 0x000C;
+        const int WM_COMMAND = 0xf5;
+
+        private static Process process;
+
+        private static IntPtr mainHandle;
+        private static IntPtr textHandle;
+        private static IntPtr inputHandle;
+        private static IntPtr sendHandle;
+
+        
+        
+        
+        /// <summary>
+        /// start nfk dedicated in new thread
+        /// </summary>
+        public static void Start()
+        {
+            var t = new Thread(StartNFK);
+            t.Start();
+        }
+
+
+        [STAThread]
+        private static void StartNFK()
+        {
+            // TODO: run without window on start
+            process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = Config.ServerExeFile,
+                    WorkingDirectory = Path.GetDirectoryName(Config.ServerExeFile),
+                    Arguments = "+gowindow +nosound +nfkplanet +game server +exec server +dontsavecfg software",
+                    RedirectStandardOutput = false,
+                    UseShellExecute = false,
+                    WindowStyle = ProcessWindowStyle.Minimized
+                }
+            };
+
+            process.Start();
+
+            // wait a second
+            Thread.Sleep(1000);
+
+
+            // get control handles
+            mainHandle = process.MainWindowHandle;
+
+            ShowWindow(mainHandle, SW_HIDE); // hide main window
+            textHandle = FindWindowEx(mainHandle, new IntPtr(0), "TMemo", null);
+            inputHandle = FindWindowEx(mainHandle, new IntPtr(0), "TEdit", null);
+            sendHandle = FindWindowEx(mainHandle, new IntPtr(0), "TButton", null);
+
+
+            // write console log to file
+            while (!process.HasExited)
+            {
+                Log.Push(GetOutput(textHandle));
+
+                // second interfal to fetch log from the NFK window
+                Thread.Sleep(1000);
+            }
+
+
+            Log.Error("Server crashed! Restarting...");
+            Thread.Sleep(3000);
+            Log.ClearOldText();
+            Start();
+        }
+
+        /// <summary>
+        /// Send command to NFK console
+        /// </summary>
+        /// <param name="text"></param>
+        public static void SendCommand(string text)
+        {
+            // fill input
+            SendMessage(inputHandle, WM_SETTEXT, 0, new StringBuilder(text));
+            // click send
+            SendMessage(sendHandle, WM_COMMAND, 0, null);
+        }
+
+        /// <summary>
+        /// Return output from NFK console
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <returns></returns>
+        private static string GetOutput(IntPtr hWnd)
+        {
+            int Len = SendMessage(hWnd, WM_GETTEXTLENGTH, 0, null) + 1;
+
+            var text = new System.Text.StringBuilder(Len);  // or length from call with GETTEXTLENGTH
+            int RetVal = SendMessage(hWnd, WM_GETTEXT, text.Capacity, text);
+
+            return text.ToString();
+        }
+
+        ~Server()
+        {
+            // kill nfk on program exit
+            if (!process.HasExited)
+                process.Kill();
+        }
+    }
+}
